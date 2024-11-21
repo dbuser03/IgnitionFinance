67777929f1bd977698c00c70ecf8c00af7427e06
Daniele Buser <d.buser@campus.unimib.it>
2024-11-21 10:16:13 +0100

    Updated package name in ui.theme package files

--- app/src/main/java/com/unimib/ignitionfinance/ui/theme/Theme.kt
+++ app/src/main/java/com/unimib/ignitionfinance/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.ignitionfinance.ui.theme
+package com.unimib.ignitionfinance.ui.theme
 
 // import android.app.Activity
 import android.os.Build
--- app/src/main/java/com/unimib/ignitionfinance/ui/theme/Type.kt
+++ app/src/main/java/com/unimib/ignitionfinance/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.ignitionfinance.ui.theme
+package com.unimib.ignitionfinance.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
