99db8620f690b9c3dbf4d3933dc1c4f8f809a83e
Daniele Buser <d.buser@campus.unimib.it>
2024-11-22 09:57:32 +0100

    Restored ui.componenst and ui.screens.intro to default

--- app/src/main/java/com/unimib/ignitionfinance/ui/components/Title.kt
+++ app/src/main/java/com/unimib/ignitionfinance/ui/components/Title.kt
@@ -2,22 +2,18 @@ package com.unimib.ignitionfinance.ui.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.unimib.ignitionfinance.ui.theme.*
 
 @Composable
 fun Title(
     title: String,
-    modifier: Modifier = Modifier
 ) {
     Text(
         text = title,
-        style = TypographyBold.headlineLarge, // Adjust to use your desired font style
+        style = TypographyBold.headlineLarge,
         textAlign = TextAlign.Left,
-        modifier = modifier
     )
 }
 
