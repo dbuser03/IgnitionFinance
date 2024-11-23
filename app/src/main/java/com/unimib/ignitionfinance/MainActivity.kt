8c43cc121219243942e23d2ac70f0a6883278cff
Daniele Buser <d.buser@campus.unimib.it>
2024-11-24 00:52:06 +0100

    Updated MainActivity.kt to show IntroScreen as the entry point

--- app/src/main/java/com/unimib/ignitionfinance/MainActivity.kt
+++ app/src/main/java/com/unimib/ignitionfinance/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.unimib.ignitionfinance.ui.screens.IntroScreen
 import com.unimib.ignitionfinance.ui.theme.IgnitionFinanceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,30 +15,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IgnitionFinanceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                IntroScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.primary,
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IgnitionFinanceTheme {
-        Greeting("Android")
+        IntroScreen()
     }
 }
