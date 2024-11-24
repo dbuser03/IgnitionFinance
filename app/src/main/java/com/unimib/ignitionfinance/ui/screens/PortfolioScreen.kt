0153f99d1670e34fe3a44eb6bbcc625e76c75cac
Daniele Buser <d.buser@campus.unimib.it>
2024-11-24 16:40:31 +0100

    Added the title in the portfolio screen

--- app/src/main/java/com/unimib/ignitionfinance/ui/screens/PortfolioScreen.kt
+++ app/src/main/java/com/unimib/ignitionfinance/ui/screens/PortfolioScreen.kt
@@ -1,2 +1,31 @@
 package com.unimib.ignitionfinance.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.unimib.ignitionfinance.ui.components.Title
+import com.unimib.ignitionfinance.ui.theme.IgnitionFinanceTheme
+
+@Composable
+fun PortfolioScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Title(title = "My \nPortfolio")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PortfolioScreenPreview() {
+    IgnitionFinanceTheme {
+        PortfolioScreen()
+    }
+}
