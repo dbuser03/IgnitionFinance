d947a8d5c9af4ab0d48be843141a7211151b0daf
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-11-25 08:49:43 +0100

    Renamed ExchangeApiServiceTest class

--- app/src/test/java/com/unimib/ignitionfinance/data/remote/exchange_api/ExchangeApiServiceTest.kt
+++ app/src/test/java/com/unimib/ignitionfinance/data/remote/exchange_api/ExchangeApiServiceTest.kt
@@ -14,7 +14,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import java.util.concurrent.TimeUnit
 
-class NewInflationApiServiceTest {
+class ExchangeApiServiceTest {
 
     private lateinit var mockWebServer: MockWebServer
     private lateinit var inflationApiService: InflationApiService
