52884f605aa170006f2cac67cdd80adbf0ae5fab
Teto03 <bianchi.teto@gmail.com>
2024-11-30 17:34:07 +0100

    Created The Authentication variable to trace the authentication parameters, and added the function to inizialize firebase Auth

--- app/src/main/java/com/unimib/ignitionfinance/Auth/EmailPasswordActivity.kt
+++ app/src/main/java/com/unimib/ignitionfinance/Auth/EmailPasswordActivity.kt
@@ -8,3 +8,15 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
+
+class EmailPasswordActivity : Activity() {
+
+    // Firebase Authentication instance
+    private lateinit var auth: FirebaseAuth
+
+    // Lifecycle method called when the activity is created
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Firebase Authentication
+        auth = Firebase.auth
+    }
