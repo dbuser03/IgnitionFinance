4bda474d8e8d02b9c00c33a80de2f42c98472279
Teto03 <bianchi.teto@gmail.com>
2024-11-30 17:32:26 +0100

    Added Imports to make the authentication logic

--- app/src/main/java/com/unimib/ignitionfinance/Auth/EmailPasswordActivity.kt
+++ app/src/main/java/com/unimib/ignitionfinance/Auth/EmailPasswordActivity.kt
@@ -1,2 +1,10 @@
 package com.unimib.ignitionfinance.Auth
 
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
