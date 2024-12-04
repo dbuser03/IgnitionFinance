cbe104a0829f62440c5092231cf866d9c28b34fb
Teto03 <bianchi.teto@gmail.com>
2024-12-04 15:02:46 +0100

    Removed all comments, this is the fondamental commit of the project

--- app/src/main/java/com/unimib/ignitionfinance/data/remote/service/ExchangeService.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/remote/service/ExchangeService.kt
@@ -1,6 +1,6 @@
 package com.unimib.ignitionfinance.data.remote.service
 
-import com.unimib.ignitionfinance.data.remote.response.ExchangeResponse  // Corrected import
+import com.unimib.ignitionfinance.data.remote.response.ExchangeResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,5 +14,5 @@ interface ExchangeService {
         @Path("seriesKey") seriesKey: String,
         @Query("format") format: String = "jsondata",
         @Query("lastNObservations") lastNObservations: Int = 1
-    ): Response<ExchangeResponse>  // Changed to match the correct response type
+    ): Response<ExchangeResponse>
 }
