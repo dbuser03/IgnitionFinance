bc919b0771d45fe0ab1d97b1a2a11469ca45aa88
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-06 10:40:59 +0100

    Fixed LoginScreen

--- app/src/main/java/com/unimib/ignitionfinance/presentation/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/unimib/ignitionfinance/presentation/ui/screens/LoginScreen.kt
@@ -1,20 +1,16 @@
 package com.unimib.ignitionfinance.presentation.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.unimib.ignitionfinance.R
-import com.unimib.ignitionfinance.presentation.ui.components.CustomFAB
 import com.unimib.ignitionfinance.presentation.ui.components.title.TitleWithDescription
-import com.unimib.ignitionfinance.presentation.navigation.Destinations
+import com.unimib.ignitionfinance.presentation.ui.components.registration.RegistrationForm
 import com.unimib.ignitionfinance.presentation.ui.theme.IgnitionFinanceTheme
 
 @Composable
@@ -23,29 +19,14 @@ fun LoginScreen(navController: NavController) {
         topBar = {
             TitleWithDescription(title = stringResource(id = R.string.app_title), description = stringResource(id = R.string.login_description))
         },
-        floatingActionButton = {
-            CustomFAB(
-                onClick = {
-                    navController.navigate(Destinations.PortfolioScreen.route){
-                        popUpTo(Destinations.LoginScreen.route) {
-                            inclusive = true
-                        }
-                    launchSingleTop = true
-                }
-                          },
-                modifier = Modifier
-                    .padding(bottom = 24.dp),
-                icon = painterResource(id = R.drawable.outline_keyboard_arrow_right_24),
-                contentDescription = stringResource(id = R.string.login_FAB_description)
-            )
-        },
-        floatingActionButtonPosition = FabPosition.Center,
         content = { innerPadding ->
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
-            )
+            ) {
+                RegistrationForm(navController)
+            }
         }
     )
 }
