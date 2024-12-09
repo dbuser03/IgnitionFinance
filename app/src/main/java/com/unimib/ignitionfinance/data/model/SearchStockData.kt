dcf6613747354072ebfdb3059cb0080c7dc3ad10
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-09 22:19:54 +0100

    Created SearchStockData

---
+++ app/src/main/java/com/unimib/ignitionfinance/data/model/SearchStockData.kt
@@ -0,0 +1,4 @@
+package com.unimib.ignitionfinance.data.model
+
+class SearchStockData {
+}
--- app/src/main/java/com/unimib/ignitionfinance/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/remote/RetrofitClient.kt
@@ -2,6 +2,7 @@ package com.unimib.ignitionfinance.data.remote
 
 import com.unimib.ignitionfinance.data.remote.service.ExchangeService
 import com.unimib.ignitionfinance.data.remote.service.StockService
+import com.unimib.ignitionfinance.data.remote.service.SearchStockService
 import com.unimib.ignitionfinance.data.remote.service.InflationService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,7 +22,7 @@ object RetrofitClient {
     private const val EXCHANGE_API_BASE_URL = "http://data-api.ecb.europa.eu/"
 
     val stockService: StockService = createRetrofit(STOCK_API_BASE_URL).create(StockService::class.java)
-    //val searchStockService: SearchStockService = createRetrofit(SEARCH_STOCK_API_BASE_URL).create(SearchStockService::class.java)
+    val searchStockService: SearchStockService = createRetrofit(SEARCH_STOCK_API_BASE_URL).create(SearchStockService::class.java)
     val inflationService: InflationService = createRetrofit(INFLATION_API_BASE_URL).create(InflationService::class.java)
     val exchangeService: ExchangeService = createRetrofit(EXCHANGE_API_BASE_URL).create(ExchangeService::class.java)
 }
