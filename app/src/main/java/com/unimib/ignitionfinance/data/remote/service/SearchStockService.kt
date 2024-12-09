7c24dbb253cad0084ca3357787569baa70425a44
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-09 22:04:22 +0100

    Fixed SearchStockService

--- app/src/main/java/com/unimib/ignitionfinance/data/remote/service/SearchStockService.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/remote/service/SearchStockService.kt
@@ -1,5 +1,17 @@
 package com.unimib.ignitionfinance.data.remote.service
 
+import com.unimib.ignitionfinance.data.remote.response.SearchStockResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface SearchStockService {
 
+    @GET("query")
+    suspend fun getSearchStockData(
+        @Query("function") function: String = "SYMBOL_SEARCH",
+        @Query("keywords") symbol: String,
+        @Query("datatype") datatype: String = "json",
+        @Query("apikey") apiKey: String
+    ): Response<SearchStockResponse>
 }
