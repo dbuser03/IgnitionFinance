9a576ae16c4d143880217c0d7e896461a251a2ec
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-11 22:01:44 +0100

    Fixed UserData

--- app/src/main/java/com/unimib/ignitionfinance/data/local/entity/UserData.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/entity/UserData.kt
@@ -1,4 +1,16 @@
 package com.unimib.ignitionfinance.data.local.entity
 
-class UserData {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.unimib.ignitionfinance.data.model.AuthData
+import com.unimib.ignitionfinance.data.model.Settings
+
+@Entity(tableName = "users")
+data class UserData (
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "surname") val surname: String?,
+    @ColumnInfo(name = "authData") val authData: AuthData,
+    @ColumnInfo(name = "settings") val settings: Settings
+)
