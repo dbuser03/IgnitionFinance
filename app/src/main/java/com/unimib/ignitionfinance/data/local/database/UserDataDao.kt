858114c6fdcdef2a48c39fa1e6a7d34894c6de1c
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-11 22:12:38 +0100

    Added insert in UserDataDao

--- app/src/main/java/com/unimib/ignitionfinance/data/local/database/UserDataDao.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/database/UserDataDao.kt
@@ -1,4 +1,11 @@
 package com.unimib.ignitionfinance.data.local.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import com.unimib.ignitionfinance.data.local.entity.UserData
+
+@Dao
 interface UserDataDao {
+    @Insert
+    fun insertAll(user: UserData)
 }
