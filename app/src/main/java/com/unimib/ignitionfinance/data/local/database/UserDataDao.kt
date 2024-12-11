9cfab455672879f76fdce7d13aa8ac36edc246eb
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-11 22:13:28 +0100

    Added delete in UserDataDao

--- app/src/main/java/com/unimib/ignitionfinance/data/local/database/UserDataDao.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/database/UserDataDao.kt
@@ -1,11 +1,17 @@
 package com.unimib.ignitionfinance.data.local.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import com.unimib.ignitionfinance.data.local.entity.UserData
 
 @Dao
 interface UserDataDao {
     @Insert
-    fun insertAll(user: UserData)
+    fun insert(user: UserData)
+
+    @Delete
+    fun delete(user: UserData)
+
+
 }
