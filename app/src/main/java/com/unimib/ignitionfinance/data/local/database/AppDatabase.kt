4f5734d70995a5a6471f67d27353b1e00a3fb218
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2024-12-11 22:45:44 +0100

    Fixed AppDatabase

--- app/src/main/java/com/unimib/ignitionfinance/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/database/AppDatabase.kt
@@ -1,4 +1,10 @@
 package com.unimib.ignitionfinance.data.local.database
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.unimib.ignitionfinance.data.local.entity.UserData
+
+@Database(entities = [UserData::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDataDao(): UserDataDao
 }
