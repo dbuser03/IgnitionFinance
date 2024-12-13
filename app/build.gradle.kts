b08889e6ff58265bff5203a0f30ef3abf0971e36
Teto03 <bianchi.teto@gmail.com>
2024-12-13 17:10:26 +0100

    Modified dependencies and manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.androidx.storage)
+    implementation(libs.play.services.base)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
