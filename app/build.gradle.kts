9ce7ae20c6b2aaf2b717bcba34e5d3ba120ed89c
dbuser03 <d.buser@campus.unimib.it>
2024-12-14 13:14:21 +0100

    Updated gradle configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.unimib.ignitionfinance"
         minSdk = 24
-        targetSdk = 34
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
