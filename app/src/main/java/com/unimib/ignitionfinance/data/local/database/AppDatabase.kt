1b40109e834bd9dd3c513baf5369ee4df6fb5a02
dbuser03 <s.brighenti4@campus.unimib.it>
2024-12-17 15:05:29 +0100

    Updated db version

--- app/src/main/java/com/unimib/ignitionfinance/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/database/AppDatabase.kt
@@ -13,7 +13,7 @@ import com.unimib.ignitionfinance.data.local.entity.User
         User::class,
         SyncQueueItem::class
     ],
-    version = 2
+    version = 3
 )
 @TypeConverters(
     UserConverter::class,
