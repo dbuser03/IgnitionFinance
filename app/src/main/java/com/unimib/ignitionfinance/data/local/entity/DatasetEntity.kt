4a51ee2c886d845869d0a5d7a0c4ae409ec3bee6
Teto03 <bianchi.teto@gmail.com>
2025-01-21 14:51:20 +0100

    Created Dataset Entity

---
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/entity/DatasetEntity.kt
@@ -0,0 +1,14 @@
+package com.unimib.ignitionfinance.data.local.entity
+
+import androidx.room.ColumnInfo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.math.BigDecimal
+
+@Entity(tableName = "datasets")
+data class DatasetEntity(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "dates") val dates: List<String>,
+    @ColumnInfo(name = "weighted_returns") val weightedReturns: List<BigDecimal>
+)
