6fc88958f53e104b110d45cb6844b0acdef34bd3
Teto03 <bianchi.teto@gmail.com>
2025-01-21 19:07:05 +0100

    Added some fixes

--- app/src/main/java/com/unimib/ignitionfinance/domain/validation/DatasetValidator.kt
+++ app/src/main/java/com/unimib/ignitionfinance/domain/validation/DatasetValidator.kt
@@ -1,23 +1,36 @@
 package com.unimib.ignitionfinance.domain.validation
 
-
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.unimib.ignitionfinance.domain.validation.utils.ValidationErrors
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 object DatasetValidator {
+
+    @RequiresApi(Build.VERSION_CODES.O)
     fun validate(historicalData: Map<String, List<Map<String, Any>>>): DatasetValidationResult {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val currentDate = LocalDate.now()
 
         for (productData in historicalData.values) {
             if (productData.isNotEmpty()) {
-                val firstDate = LocalDate.parse(productData.first()["date"] as String, formatter)
-                if (firstDate.isBefore(currentDate.minusYears(10))) {
-                    return DatasetValidationResult.Success
+                val firstDateStr = productData.first()["date"] as? String
+                if (firstDateStr.isNullOrBlank()) {
+                    return DatasetValidationResult.Failure(ValidationErrors.Input.INVALID_INPUT.format("date"))
+                }
+
+                try {
+                    val firstDate = LocalDate.parse(firstDateStr, formatter)
+                    if (firstDate.isBefore(currentDate.minusYears(10))) {
+                        return DatasetValidationResult.Success
+                    }
+                } catch (e: Exception) {
+                    return DatasetValidationResult.Failure(ValidationErrors.Input.INVALID_INPUT.format("date"))
                 }
             }
         }
-        return DatasetValidationResult.Failure("No product with more than 10 years of historical data found")
+        return DatasetValidationResult.Failure(ValidationErrors.Input.YEARS_LIMIT)
     }
 }
 
