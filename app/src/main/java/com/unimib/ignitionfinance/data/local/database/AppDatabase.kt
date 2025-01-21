7551975acc1d74f6aeabdde2540f78392fce3c7d
Teto03 <bianchi.teto@gmail.com>
2025-01-21 15:30:59 +0100

    Added the converter to the database

--- app/src/main/java/com/unimib/ignitionfinance/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/unimib/ignitionfinance/data/local/database/AppDatabase.kt
@@ -3,6 +3,7 @@ package com.unimib.ignitionfinance.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.unimib.ignitionfinance.data.local.converter.DatasetConverter
 import com.unimib.ignitionfinance.data.local.converter.SyncQueueItemConverter
 import com.unimib.ignitionfinance.data.local.converter.UserConverter
 import com.unimib.ignitionfinance.data.local.entity.Dataset
@@ -20,7 +21,8 @@ import com.unimib.ignitionfinance.data.local.entity.User
 )
 @TypeConverters(
     UserConverter::class,
-    SyncQueueItemConverter::class
+    SyncQueueItemConverter::class,
+    DatasetConverter::class
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
