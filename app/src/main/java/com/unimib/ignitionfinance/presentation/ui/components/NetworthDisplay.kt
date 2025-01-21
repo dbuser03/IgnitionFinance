117a7dcccef033667473c730dca313823bf6e5ee
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-01-21 16:46:39 +0100

    Resolved some warnings

--- app/src/main/java/com/unimib/ignitionfinance/presentation/ui/components/NetworthDisplay.kt
+++ app/src/main/java/com/unimib/ignitionfinance/presentation/ui/components/NetworthDisplay.kt
@@ -1,8 +1,5 @@
-@file:Suppress("UNREACHABLE_CODE")
-
 package com.unimib.ignitionfinance.presentation.ui.components
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
