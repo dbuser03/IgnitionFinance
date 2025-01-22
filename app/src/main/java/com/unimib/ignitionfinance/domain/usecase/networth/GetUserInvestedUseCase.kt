3d76295e65d9de3d6b4b626accbd5f51d9b2b9c7
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-01-22 12:27:35 +0100

    Deleted useless file

--- app/src/main/java/com/unimib/ignitionfinance/domain/usecase/networth/GetUserInvestedUseCase.kt
+++ app/src/main/java/com/unimib/ignitionfinance/domain/usecase/networth/GetUserInvestedUseCase.kt
@@ -1,5 +1,5 @@
 package com.unimib.ignitionfinance.domain.usecase.networth
 
 class GetUserInvestedUseCase {
-
+    //in input una product list e ritornare la somma di amount per ogni singolo prodotto
 }
