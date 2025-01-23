bc412e61e3fef5c1ce7300a12660824ddcc7d5d4
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-01-23 14:30:57 +0100

    Finally fixed SummaryScreenViewModel

--- app/src/main/java/com/unimib/ignitionfinance/presentation/viewmodel/SummaryScreenViewModel.kt
+++ app/src/main/java/com/unimib/ignitionfinance/presentation/viewmodel/SummaryScreenViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-@HiltViewModel
+//@HiltViewModel
 class SummaryScreenViewModel @Inject constructor (
     private val getUserCashUseCase: GetUserCashUseCase,
     private val getUserInvestedUseCase: GetUserInvestedUseCase
