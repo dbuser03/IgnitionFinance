af300f190e4626057f643074f41498d7da76f3b5
Teto03 <bianchi.teto@gmail.com>
2025-01-28 10:38:49 +0100

    Removed comments

--- app/src/main/java/com/unimib/ignitionfinance/domain/usecase/FetchHistoricalDataUseCase.kt
+++ app/src/main/java/com/unimib/ignitionfinance/domain/usecase/FetchHistoricalDataUseCase.kt
@@ -12,37 +12,27 @@ class FetchHistoricalDataUseCase @Inject constructor(
     private val getProductListUseCase: GetProductListUseCase,
     private val stockRepository: StockRepository
 ) {
-    // Parametro esterno per la chiave API, per flessibilità e sicurezza
     fun execute(apiKey: String): Flow<Result<List<Map<String, StockData>>>> = flow {
         try {
-            // Recupera prima la lista dei prodotti
             val productsResult = getProductListUseCase.execute().first()
 
-            // Gestisci il risultato della lista prodotti
             val products = productsResult.getOrElse {
-                // In caso di errore, emetti un errore
                 emit(Result.failure(it))
                 return@flow
             }
 
-            // Mappa per contenere gli storici dei prodotti
             val historicalDataList = mutableListOf<Map<String, StockData>>()
 
-            // Recupera lo storico per ogni prodotto
             products.forEach { product ->
-                // Recupera lo storico per il singolo simbolo
                 val stockDataResult = stockRepository.fetchStockData(product.symbol, apiKey).first()
 
                 stockDataResult.onSuccess { stockData ->
                     historicalDataList.add(stockData)
                 }.onFailure {
-                    // Gestisci eventuali errori per un singolo prodotto
-                    // Puoi scegliere di lanciare un'eccezione o continuare
                     emit(Result.failure(it))
                 }
             }
 
-            // Emetti il risultato finale
             emit(Result.success(historicalDataList))
 
         } catch (e: Exception) {
