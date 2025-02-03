8f448f61fff6ac12bc65f4da0294cd50973521e1
Teto03 <bianchi.teto@gmail.com>
2025-02-03 16:09:30 +0100

    Added the test dataset usecase

---
+++ app/src/main/java/com/unimib/ignitionfinance/domain/usecase/BuildDatasetOnlyUseCase.kt
@@ -0,0 +1,27 @@
+package com.unimib.ignitionfinance.domain.usecase
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class BuildDatasetOnlyUseCase @Inject constructor(
+    private val buildDatasetUseCase: BuildDatasetUseCase
+) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun execute(apiKey: String): Flow<Result<Unit>> = flow {
+        try {
+            // Step 1: Build the dataset...
+            val datasetResult = buildDatasetUseCase.execute(apiKey).first()
+            datasetResult.getOrElse {
+                emit(Result.failure(it))
+                return@flow
+            }
+            emit(Result.success(Unit))
+        } catch (e: Exception) {
+            emit(Result.failure(e))
+        }
+    }
+}
