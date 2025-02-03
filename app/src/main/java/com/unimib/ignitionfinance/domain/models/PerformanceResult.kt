e7acdf13cadfc71f6fdf2d69c1a9b0cb2ea5cfb7
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-02-03 22:58:14 +0100

    Fixed PerformanceResult

--- app/src/main/java/com/unimib/ignitionfinance/domain/models/PerformanceResult.kt
+++ app/src/main/java/com/unimib/ignitionfinance/domain/models/PerformanceResult.kt
@@ -1,2 +1,7 @@
 package com.unimib.ignitionfinance.domain.models
 
+data class PerformanceResult(
+    val successRate: Double,
+    val mediaTotale: Double,
+    val deviazioneStandardTotale: Double
+)
