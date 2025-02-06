0c75a74b5538504465478fcdb4901670706bfa03
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-02-06 14:56:11 +0100

    Fixed all warnings

--- app/src/main/java/com/unimib/ignitionfinance/presentation/ui/components/summary/PerformanceCard.kt
+++ app/src/main/java/com/unimib/ignitionfinance/presentation/ui/components/summary/PerformanceCard.kt
@@ -7,10 +7,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.unimib.ignitionfinance.R
-import com.unimib.ignitionfinance.presentation.ui.theme.IgnitionFinanceTheme
 import com.unimib.ignitionfinance.presentation.ui.theme.TypographyMedium
 
 @Composable
