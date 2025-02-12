60d82742733ac89fc79a3803fb3707e229a7d314
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-02-13 00:13:02 +0100

    Fixed warnings in InputBoxBody

--- app/src/main/java/com/unimib/ignitionfinance/presentation/ui/components/settings/input/InputBoxBody.kt
+++ app/src/main/java/com/unimib/ignitionfinance/presentation/ui/components/settings/input/InputBoxBody.kt
@@ -1,10 +1,5 @@
 package com.unimib.ignitionfinance.presentation.ui.components.settings.input
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
