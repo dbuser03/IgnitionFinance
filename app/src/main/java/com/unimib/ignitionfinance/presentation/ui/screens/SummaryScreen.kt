d160f763bdd4e5a801264f918ef23ccfcc9c6a2b
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-02-13 01:46:31 +0100

    Removed CalculateHoldingPeriodInYears from SummaryScreen

--- app/src/main/java/com/unimib/ignitionfinance/presentation/ui/screens/SummaryScreen.kt
+++ app/src/main/java/com/unimib/ignitionfinance/presentation/ui/screens/SummaryScreen.kt
@@ -215,17 +215,3 @@ data class Quadruple<A, B, C, D>(
     val third: C,
     val fourth: D
 )
-
-/*@RequiresApi(Build.VERSION_CODES.O)
-private fun calculateHoldingPeriodInYears(purchaseDate: String): Double {
-    val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-    val now = LocalDate.now()
-
-    try {
-        val parsedDate = LocalDate.parse(purchaseDate, dateFormatter)
-        val daysBetween = ChronoUnit.DAYS.between(parsedDate, now)
-        return daysBetween / 365.0
-    } catch (_: DateTimeParseException) {
-        return 0.0
-    }
-}*/
